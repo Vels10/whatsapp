32a8436874670dfff630d36867fed54e2f7726b0
Velkumaran AP <38457682+Vels10@users.noreply.github.com>
2019-08-23 10:17:38 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome("C:/Users/vels10/Downloads/chromedriver.exe")
-driver.get('https://web.whatsapp.com/')
+driver = webdriver.Chrome("C:/Users/vels10/Downloads/chromedriver.exe")     # Download Chromedriver and initialize with the path                      
+driver.get('https://web.whatsapp.com/')                                     # Browser for Whatsapp
 
-name = input('Enter the name of user or group : ')
-msg = input('Enter your message : ')
-count = int(input('Enter the count : '))
+name = input('Enter the name of user or group : ')                          # Group Name
+msg = input('Enter your message : ')                                        # Enter the Message you want to send
+count = int(input('Enter the count : '))                                    # N number of messages it send
 
-input('Enter anything after scanning QR code')
+input('Enter anything after scanning QR code')                                  
 
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))   # xpath of the group name
 user.click()
 
-msg_box = driver.find_element_by_class_name('_13mgZ')
+msg_box = driver.find_element_by_class_name('_13mgZ')                       #(div -> class name ('_13mgz')) Eg: it will be (<div tabindex "-1" class = "_13mgZ"> == $0)
  
 
 for i in range(count):
     msg_box.send_keys(msg)
-    button = driver.find_element_by_class_name('_3M-N-')
+    button = driver.find_element_by_class_name('_3M-N-')                    #Send Button Class name Eg: <button class = "_3M-N-">...</button> == $0
     button.click()
 
